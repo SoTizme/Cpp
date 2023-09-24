e1046f9e2168202c066758a3ecbf77f06fb23417
Souad Hilal <shilal@c3r10p8.1337.ma>
2023-09-24 20:50:35 +0100

    ex02 almost complite

--- ex02/Contact.hpp
+++ ex02/Contact.hpp
@@ -3,16 +3,16 @@
 
 class Contact {
 
+
+    public :
+        void set_contact(int index, std::string a1, std::string b1, std::string c1, std::string d1, std::string e1);
+        void get_contact(int index);
     int         index;
     std::string  first_name;
     std::string  last_name;
     std::string  nickname;
     std::string  phone_number;
     std::string  darkest_secret;
-
-    public :
-        void set_contact(int index, std::string a1, std::string b1, std::string c1, std::string d1, std::string e1);
-        void get_contact(int index);
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,21 +4,17 @@
 PhoneBook::PhoneBook(void)
 {
 	this->id = 0;
-   std::cout << "-----------------------------\n";
-   std::cout << "--Welecome To The PhoneBook--\n";
-   std::cout << "-----------------------------\n";
+
+	std::cout << "_________________________________________" << std::endl;
+	std::cout << "~~~~~~> Welecome To The PhoneBook <~~~~~~" << std::endl;
+	std::cout << "" << std::endl;
 }
 
 PhoneBook::~PhoneBook(void)
 {
-   std::cout << "-----------------------------\n";
-   std::cout << "-----------Good by-----------\n";
-   std::cout << "-----------------------------\n";
-}
-
-void    PhoneBook::ft_search(void)
-{
-
+   std::cout << "-----------------------------" << std::endl;
+   std::cout << "-----------Good by-----------" << std::endl;
+   std::cout << "-----------------------------" << std::endl;
 }
 
 void Contact::set_contact(int id, std::string a, std::string b, std::string c, std::string d, std::string e)
@@ -31,10 +27,6 @@ void Contact::set_contact(int id, std::string a, std::string b, std::string c, s
 	this->darkest_secret = e;
 }
 
-void Contact::get_contact(int index){
-
-}
-
 void    PhoneBook::ft_add(void)
 {
 	std::cout << "Enter first name >>"; getline(std::cin, a);
@@ -42,14 +34,23 @@ void    PhoneBook::ft_add(void)
 	std::cout << "Enter nickname >>"; getline(std::cin, c);
 	std::cout << "Enter  phone number >>"; getline(std::cin, d);
 	std::cout << "Enter darkest secret >>"; getline(std::cin, e);
+	for (int i = 0; d[i]; i++)
+	{
+		if (isdigit(d[i]) == 0)
+		{
+			std::cout << "----------Error in Number Phone---------" << std::endl;
+			return ;
+		}
+	}
 	if (PhoneBook::a == "" || PhoneBook::b == "" || PhoneBook::c == "" 
 		|| PhoneBook::d == "" || PhoneBook::e == "")
-		std::cout << "--------------Empty filed---------------\n";
+		std::cout << "--------------Empty filed---------------"<< std::endl;
 	else
 	{
 		this->id %= 8;
 		this->Tab[this->id].set_contact(this->id, a, b, c, d, e);
 		this->id++;
+		std::cout << "------The Contact ADD in Successfuly------"<< std::endl;
 	}
 	return ;
 }
@@ -57,8 +58,22 @@ void    PhoneBook::ft_add(void)
 void    PhoneBook::ft_search(void)
 {
 	std::string s;
-	std::cout << "Enter The index of the Cantact>>"; getline(std::cin, s);
-	for (,)
+	int	nb;
+
+	std::cout << "----------------------------------------"<< std::endl;
+	std::cout << "Number of the Cantact >> "; getline(std::cin, s);
+	if (s.length() == 1 && s.find_first_not_of("0123456789"))
+	{
+		std::cout << "its integer" << std::endl;
+		for (int i=0; s[i]; i++){
+			std::cout << s[i] << std::endl;
+			if (int(s[0] - 48) == this->Tab[i].index)
+				std::cout << "hiii" << std::endl;
+		}
+	}
+	else
+		std::cout << "----------------------------------------"<< std::endl;
+		std::cout << "------------Ther's NO Inedex------------"<< std::endl;
 }
 
 int main()
@@ -66,19 +81,17 @@ int main()
 	PhoneBook p;
 	std::string str;
 
-	std::cout << "----------------------------------------\n";
-	std::cout << "To Save a contact Use this : ADD\n";
-	std::cout << "To Serch for a contact Use this : SEARCH\n";
-	std::cout << "To Quit Use this : EXIT\n";
+	std::cout << "-------Use The Following Commnads-------"<< std::endl;
+	std::cout << "~~~~~~~~>|ADD|--|SEARCH|--|EXIT|<~~~~~~~" << std::endl;
 	while(1)
 	{
-		std::cout << "----------------------------------------\n";
-		std::cout << "Tap your command >>";
+		std::cout << "----------------------------------------" << std::endl;
+		std::cout << "Tap your command >> ";
 		std:: getline(std::cin, str);
 		if (str.compare("ADD") == 0)
-			  p.ft_add();
+			p.ft_add();
 		else if (str.compare("SEARCH") == 0)
-			std :: cout << "SEARCH\n";
+			p.ft_search();
 		else if (str.compare("EXIT") == 0)
 			break ;
 	}
