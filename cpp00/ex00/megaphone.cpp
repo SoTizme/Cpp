0c7b60b67cbbff351c0bd2c25cc441e7bbadf0b2
Souad Hilal <shilal@e1r13p10.1337.ma>
2023-10-12 05:08:10 +0100

    update ex00/cpp00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,20 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shilal <shilal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 04:37:50 by shilal            #+#    #+#             */
+/*   Updated: 2023/10/12 05:03:51 by shilal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-   
-namespace scope {
-	void to_uper(char *s){
-		for (int i = 0; s[i] ; i++)
-			std::cout << (char)toupper(s[i]);
-		std::cout << " ";
-	}
+
+void to_uper(char *s){
+	for (int i = 0; s[i] ; i++)
+		std::cout << (char)toupper(s[i]);
 }
 
 int main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"<< std::endl;
 	else{
 		for (int j = 1; j < ac; j++)
-			scope::to_uper(av[j]);
-		std::cout << "\n";
+			to_uper(av[j]);
+		std::cout << std::endl;
 	}
 }
