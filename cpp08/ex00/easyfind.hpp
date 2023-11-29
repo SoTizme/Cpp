6f9ddc0c7e13d88770958b818651ce6c7764a705
Souad Hilal <shilal@e1r2p12.1337.ma>
2023-11-29 18:04:51 +0100

    finish ex00/cpp08

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: shilal <shilal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 21:08:29 by shilal            #+#    #+#             */
-/*   Updated: 2023/11/28 16:09:42 by shilal           ###   ########.fr       */
+/*   Updated: 2023/11/29 18:03:13 by shilal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@
 #include <iostream>
 #include <vector>
 
-template <typename T> void easyfind(T g1, int i){
+template <typename T> void easyfind(T arr, int f)
+{
+    std::vector<int>::iterator n;
+    n = find(arr.begin(), arr.end(), f);
+    if (n == arr.end())
+        throw std::out_of_range("Not Found");
+    std::cout << "The occurrence Found" << std::endl;
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,12 +6,29 @@
 /*   By: shilal <shilal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 21:08:31 by shilal            #+#    #+#             */
-/*   Updated: 2023/11/28 16:09:47 by shilal           ###   ########.fr       */
+/*   Updated: 2023/11/29 18:04:06 by shilal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+#include <iostream>
+#include <vector>
+
 int main(){
-  
+    int arr[4] = {0,55,8,80};
+	std::vector <int> ptr;
+	for (int i = 0; i < 4; i++) {
+		ptr.push_back(arr[i]);
+	}
+
+	try
+	{
+		easyfind(ptr, 8);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 }
